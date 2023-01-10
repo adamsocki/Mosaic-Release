8c7951fe74834c305a2f3e36ae19867841cb439e
socki <socki@LAPTOP-DGI9IL02>
2023-01-09 21:57:18 -0600

    mipmap

--- src/render_opengl.cpp
+++ src/render_opengl.cpp
@@ -838,7 +838,9 @@ void OpenGL_InitTexture(Sprite *texture) {
     glBindTexture(GL_TEXTURE_2D, texture->textureID);
     glCheckError();
 
-    
+    glGenerateMipmap(GL_TEXTURE_2D);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
